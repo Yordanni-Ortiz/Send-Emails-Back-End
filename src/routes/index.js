88cb1290a0162f65c39419d335b16fc4b510999d
Yordanni Ortiz <yordannimod@gmail.com>
2023-04-23 19:28:25 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const sendEmail = require('../utils/sendEmail');
+
+// colocar las rutas aquí
+router.get('/', (req, res) => {
+    res.send("Welcome to express");
+});
+
+router.post('/emails/sayhi', async(req, res) => {
+    const { name, email, phone, message } = req.body;
+    await sendEmail({
+        to: "yordannimod@gmail.com",
+        subject: `¡${name} te escribió desde tu portafolio!`,
+        html: `<FONT COLOR="purple"><p>${message}</p></FONT>
+        <ul>
+        <li type="disc"><FONT COLOR="purple"><b>Phone</b> ${phone}</FONT></li>
+        <li type="disc"><FONT COLOR="purple"><b>Email</b> ${email}</FONT></li>
+        </ul> 
+    `
+    })
+    return res.json({ message: "Email sent successfuly" })
+});
+
+router.post('/emails/contact', async(req, res) =>{
+    const { name, email, phone, message } = req.body;
+    await sendEmail({
+        to: "yordannimod@gmail.com",
+        subject: `¡${name} te escribió desde tu portafolio!`,
+        html: `
+            <FONT COLOR="DarkSlateBlue"><h1>A ${name} le gustaría ponerse en contacto contigo.</h1></FONT>
+            <FONT COLOR="DarkSlateBlue"><p>${message}</p></FONT>
+            <ul>
+            <li type="disc"><FONT COLOR="DarkSlateBlue"><b>Email:</b> ${email}</FONT></li>
+            <li type="disc"><FONT COLOR="DarkSlateBlue"><b>Phone:</b> ${phone}</FONT></li>
+            </ul> 
+        `
+    })
+    return res.send("Email sent successfuly")
+})
+
+module.exports = router;
